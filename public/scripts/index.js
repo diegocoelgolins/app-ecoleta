0a8d7ee1651a39aeeb549efa71ab462dc0dba881
diego coelho <diegocoelho44@gmail.com>
2022-05-17 09:51:00 -0300

    first commit

---
+++ public/scripts/index.js
@@ -0,0 +1,11 @@
+//abrir e fechar o modal para a pesquisa de pontos de coleta
+const buttonSearch = document.querySelector("#page-home main a")
+const modal = document.querySelector("#modal")
+const close = document.querySelector("#modal .header a")
+buttonSearch.addEventListener("click", () => {
+  modal.classList.remove("hide")
+})
+
+close.addEventListener("click", () => {
+  modal.classList.add("hide")
+})
