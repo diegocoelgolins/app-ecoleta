ade32f3ee53753476d65b4502701350511592439
diego coelho <diegocoelho44@gmail.com>
2022-05-18 16:26:42 -0300

    criação de rotas

--- src/server.js
+++ src/server.js
@@ -2,5 +2,22 @@
 const express = require("express")
 const server = express()
 
+
+// configurar pasta pública
+server.use(express.static("public"))
+
+
+// configurar caminhos da minha aplicação
+// página inicial
+server.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html")
+})
+
+//criando rotas
+server.get("/page2", function(req, res) {
+  res.sendFile(__dirname + "/views/page2.html")
+})
+
+
 //ligar o servidor
 server.listen(3000)
